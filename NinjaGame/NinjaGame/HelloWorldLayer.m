c1a66e5eb1ace8b3653d55f20841837ad2fdbc92
Patrick Berens <patrickcberens@gmail.com>
2012-02-26 23:13:36 -0700

    Added sprites and sound. Add functionality up until touches(which don't work...neither does NSLog). Possible permissions issue.

--- NinjaGame/NinjaGame/HelloWorldLayer.m
+++ NinjaGame/NinjaGame/HelloWorldLayer.m
@@ -28,24 +28,99 @@
 	return scene;
 }
 
+-(void)spriteMoveFinished:(id)sender{
+    CCSprite *sprite = (CCSprite *)sender;
+    [self removeChild:sprite cleanup:YES];
+}
+-(void)addTarget{
+    CCSprite *target = [CCSprite spriteWithFile:@"Target.png" rect:CGRectMake(0, 0, 27, 40)];
+    NSLog(@"addTarget");
+    //Determine where to spawn the target along Y-axis
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    int minY = target.contentSize.height/2;
+    int maxY = winSize.height - target.contentSize.height/2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    //Create the target slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    target.position = ccp(winSize.width + (target.contentSize.width/2), actualY);
+    [self addChild:target];
+    
+    //Determine the speed of the target;
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    //Create the actions
+    id actionMove = [CCMoveTo actionWithDuration:actualDuration position:ccp(-target.contentSize.width/2, actualY)];
+    id actionMoveDone = [CCCallFuncN actionWithTarget:self selector:@selector(spriteMoveFinished:)];
+    [target runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
+    
+}
+
+-(void)ccTouchBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    //-(void)ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event{
+    NSLog(@"Detected Touch Ended");
+    //Choose one of the touches to work with
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:[touch view]];
+    location = [[CCDirector sharedDirector] convertToGL:location];
+    
+    //Set up initial location of projectile
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    CCSprite *projectile = [CCSprite spriteWithFile:@"Projectile.png" rect:CGRectMake(0, 0, 20, 20)];
+    projectile.position = ccp(20, winSize.height/2);
+    
+    //Determine offset of location to projectile
+    int offX = location.x - projectile.position.x;
+    int offY = location.y - projectile.position.y;
+    
+    //Bail out if we are shooting down or backwards
+    if(offX <= 0) return;
+    
+    //Ok to add now - we've double checked position
+    [self addChild:projectile];
+    
+    //Determine where we wish to shoot the projectile to
+    int realX = winSize.width + (projectile.contentSize.width/2);
+    float ratio = (float) offY / (float) offX;
+    int realY = (realX * ratio) + projectile.position.y;
+    CGPoint realDest = ccp(realX, realY);
+    
+    //Determine the length of how far we're shooting
+    int offRealX = realX - projectile.position.x;
+    int offRealY = realY - projectile.position.y;
+    float length = sqrtf((offRealX * offRealX) + (offRealY * offRealY));
+    float velocity = 280/1; //480 pixels per 1 second
+    float realMoveDuration = length/velocity;
+    
+    //Move projectile to actual endpoint
+    [projectile runAction:[CCSequence actions:
+                           [CCMoveTo actionWithDuration:realMoveDuration position:realDest],
+                           [CCCallFuncN actionWithTarget:self selector:@selector(spriteMoveFinished:)], nil]];
+}
+
+-(void)gameLogic:(ccTime)dt{
+    [self addTarget];
+}
+
 // on "init" you need to initialize your instance
 -(id) init
 {
 	// always call "super" init
 	// Apple recommends to re-assign "self" with the "super" return value
-	if( (self=[super init])) {
+	if( (self=[super initWithColor:ccc4(255,255,255,255)])) {
+        self.isTouchEnabled = YES;
+        NSLog(@"Entered init");
+		CGSize winSize = [[CCDirector sharedDirector] winSize];
+        CCSprite *player = [CCSprite spriteWithFile:@"Player.png" rect:CGRectMake(0, 0, 27, 40)];
+        player.position = ccp(player.contentSize.width/2, winSize.height/2);
+        [self addChild:player];
+        [self schedule:@selector(gameLogic:) interval:1.0];
+        
 		
-		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
-
-		// ask director the the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
-	
-		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
-		
-		// add the label as a child to this Layer
-		[self addChild: label];
 	}
 	return self;
 }
