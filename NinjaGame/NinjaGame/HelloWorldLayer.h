4abb9ee2bb32e16c1b98e7b73115edab491d3cbd
Patrick Berens <patrickcberens@gmail.com>
2012-02-26 23:04:48 -0700

    First Commit of Ninja Game with Cocos2D stuff

---
+++ NinjaGame/NinjaGame/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  NinjaGame
+//
+//  Created by default on 2/26/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
