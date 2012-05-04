184389e46b2cad8a2d23cc21600aff2cb912b3e8
John Bender <john@generalui.com>
2012-05-04 12:03:24 -0700

    grading

--- ninjaToArtillery/ninjaToArtillery/AppDelegate.h
+++ ninjaToArtillery/ninjaToArtillery/AppDelegate.h
@@ -18,3 +18,18 @@
 @property (nonatomic, retain) UIWindow *window;
 
 @end
+
+/*
+ Nice-looking, playable app, and extra kudos for teaching yourself Cocos2d. I'd
+ prefer if you gave credit to the Ray Wenderlich tutorial for the code you
+ borrowed, but you obviously modified it heavily and understand how it works, so
+ I'm not too concerned. Good code structure, comments, etc.
+ 
+ The app crashes strangely on a device, but not in the simulator. It says it
+ can't find Target_Jet_Blue_mod.png, though the file is present and loads in the
+ simulator. I don't know what to make of this - the iOS developer tools are buggy
+ sometimes.
+ 
+ Grade: 100%
+ Course grade: 100% (A)
+*/
